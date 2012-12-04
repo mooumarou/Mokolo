3ab698bea286a6d429d0b0f86678f91a90dafdbf
Moussa Oumarou <moussa.oumarou@stud.uni-due.de>
2012-12-04 17:44:52 +0100

    Initial commit

---
+++ GitTest/src/com/mobl/git/test/TestGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mobl.git.test;
+
+/**
+ * @author MOBL
+ * 
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("");
+	}
+
+}
